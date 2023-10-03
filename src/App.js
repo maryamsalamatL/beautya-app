13ff8b7ee433b1d9cf65f3110de755479d12a245
maryamsalamatl <maryamsalamat47@gmail.com>
2023-10-03 12:12:49 +0330

    added some line for test

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Layout from "./layout/Layout";
 import { Route, Routes } from "react-router-dom";
 import routes from "./routes";
 function App() {
+  console.log("dumyy text");
   return (
     <Layout>
       <Routes>
